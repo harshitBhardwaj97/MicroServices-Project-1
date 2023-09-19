b2500a4dc13463989bcb9c02ea107f824c3ceb7e
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2023-09-19 21:17:42 +0530

    Service Registry Complete

--- UserService/src/main/java/com/microservice1/user/controller/UserController.java
+++ UserService/src/main/java/com/microservice1/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.microservice1.user.controller;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.microservice1.user.model.Rating;
+import com.microservice1.user.model.Hotel;
 import com.microservice1.user.model.User;
 import com.microservice1.user.service.UserService;
 
@@ -22,9 +27,21 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@GetMapping("/{userId}")
 	public ResponseEntity<User> getUser(@PathVariable String userId){
 		User user = userService.getUser(userId);
+		Rating[] ratingsOfUser = restTemplate.getForObject("http://RATING-SERVICE/ratings/users/"+user.getUserId(), Rating[].class);
+		List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+		List<Rating> ratingList =  ratings.stream().map(rating -> {
+			ResponseEntity<Hotel> hotelEntity = restTemplate.getForEntity("http://HOTEL-SERVICe/hotels/"+rating.getHotelId(), Hotel.class);
+			Hotel hotelBody = hotelEntity.getBody();
+			rating.setHotel(hotelBody);
+			return rating;
+		}).collect(Collectors.toList());
+		user.setRatings(ratingList);
 		return ResponseEntity.ok(user);
 	}
 
@@ -33,7 +50,7 @@ public class UserController {
 		List<User> userList = userService.getAllUsers();
 		return ResponseEntity.ok(userList);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<User> createUser(@RequestBody User user){
 		User createdUser = userService.createUser(user);
